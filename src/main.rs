4313459d275d2f85a1bd72e8389442a0e145f94c
Choi Junyoung <juny0955@gmail.com>
2026-01-20 21:37:23 +0900

    기본설정

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .json()
+        .init();
+
+    info!("Application started");
+
+    Ok(())
 }
