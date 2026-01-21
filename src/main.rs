effb2bda6f71104cc45aee15ebe4c6022e256d60
juny0955 <juny0955@gmail.com>
2026-01-21 10:15:04 +0900

    yaml 세팅 설정

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-use anyhow::Result;
+mod settings;
+
+use anyhow::{Context, Result};
 use tracing::info;
+use tracing_subscriber::EnvFilter;
+use crate::settings::Settings;
 
 #[tokio::main]
 async fn main() -> Result<()>{
     tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .json()
+        .with_env_filter(
+            EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| EnvFilter::new("info"))
+        )
         .init();
 
-    info!("Application started");
+    let settings = Settings::load_settings().context("설정 파일 로드 실패")?;
+    info!("settings loaded");
+    info!("{} started", settings.name);
 
     Ok(())
 }
---
+++ src/settings.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use config::{Config, File};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    pub name: String,
+    pub sources: Vec<SourceSettings>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SourceSettings {
+    pub label: String,
+    pub path: String,
+}
+
+impl Settings {
+    pub fn load_settings() -> Result<Self> {
+        let settings = Config::builder()
+            .add_source(File::with_name("config/agent.yaml"))
+            .build()?
+            .try_deserialize()?;
+
+        Ok(settings)
+    }
+}
